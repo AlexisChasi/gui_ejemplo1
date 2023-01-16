88d5d84e602530f471b2cb8a820d88c3b48dc2c5
AlexisChasi <alexis.chasi@epn.edu.ec>
2023-01-16 11:23:17 -0500

    con manejo de exepcion

--- src/dialogo.java
+++ src/dialogo.java
@@ -1,13 +1,18 @@
 import javax.swing.JOptionPane;
 public class dialogo {
     public static void main(String[] args) {
-        double peso=
-                Double.parseDouble(JOptionPane.showInputDialog("Ingrese el peso en KG"));
-        double altura=
-                Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura  en m"));
-        double imc=peso/(altura*altura);
+        try {
+            double peso=
+                    Double.parseDouble(JOptionPane.showInputDialog("Ingrese el peso en KG"));
+            double altura=
+                    Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura  en m"));
+            double imc=peso/(altura*altura);
 
-        JOptionPane.showMessageDialog(null,"su ICM es de :"+imc);
+            JOptionPane.showMessageDialog(null,"su ICM es de :"+imc);
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Ingrese bien los datos!!");
+        }
+
     }
 
 }
