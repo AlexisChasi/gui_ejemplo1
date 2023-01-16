f1fd9abf1416d335a09997922d93cc7e5c976fc5
AlexisChasi <alexis.chasi@epn.edu.ec>
2023-01-16 11:13:12 -0500

    CALCULO IMC

---
+++ src/dialogo.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+public class dialogo {
+    public static void main(String[] args) {
+        double peso=
+                Double.parseDouble(JOptionPane.showInputDialog("Ingrese el peso en KG"));
+        double altura=
+                Double.parseDouble(JOptionPane.showInputDialog("Ingrese la altura  en m"));
+        double imc=peso/altura;
+        String mensaje=String.format("su ICM es de :",imc);
+        JOptionPane.showMessageDialog(null,imc);
+    }
+
+}
